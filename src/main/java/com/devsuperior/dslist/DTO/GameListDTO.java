e64d5dcae4db92bdd2fa1a4af9b2b84d645fdac5
Newton Fernandes <newtonfernandesti@gmail.com>
2023-05-10 12:37:37 -0300

    GET lists

---
+++ src/main/java/com/devsuperior/dslist/DTO/GameListDTO.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dslist.DTO;
+
+import com.devsuperior.dslist.entities.GameList;
+
+public class GameListDTO {
+
+	private long id;
+	private String name;
+	
+	public GameListDTO(){
+		
+	}
+
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
